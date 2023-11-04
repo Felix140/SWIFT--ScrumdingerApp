64ca380e3728fddefb2dbbd025f96655b86bd4dc
Felix Valdez <fdvaldez14@gmail.com>
2023-11-04 02:16:39 +0100

    Add right label style

---
+++ Scrumdinger/Scrumdinger/Models/RightIconLabelStyle.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+// Stile per le icone di SX
+struct RightIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == RightIconLabelStyle{
+    static var rightIcon: Self { Self() }
+}
