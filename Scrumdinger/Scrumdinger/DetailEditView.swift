aedd7b4ae822e9664fb79ac178a31de8fc0931ea
Felix Valdez <fdvaldez14@gmail.com>
2023-11-08 23:29:24 +0100

    Add another section(Attendee)

--- Scrumdinger/Scrumdinger/DetailEditView.swift
+++ Scrumdinger/Scrumdinger/DetailEditView.swift
@@ -2,9 +2,11 @@ import SwiftUI
 
 struct DetailEditView: View {
     @State private var scrum = DailyScrum.emptyScrum // sono @State tutte le variabili MUTABILI (observed)
+    @State private var newAttendeeName = "" // holds the attendee name that the user enters
     
     var body: some View {
         Form {
+            
             Section(header: Text("Meeting Info")) {
                 TextField("Title", text: $scrum.title) // the $ syntax create a binding to scrum.title
                 HStack {
@@ -13,6 +15,22 @@ struct DetailEditView: View {
                     }
                     Spacer()
                     Text("\(scrum.lengthInMinutes) minutes")
+                }
+            }
+            
+            Section(header: Text("Partecipanti")) {
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices) //remove attendees from the scrum data.
+                }
+                
+                HStack {
+                    TextField("Nuovo partecipante", text: $newAttendeeName)
+                    Button(action: {}) {
+                        Image(systemName: "plus.circle.fill")
+                    }
                 }
             }
         }
