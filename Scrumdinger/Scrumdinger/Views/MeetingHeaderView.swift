cc19ee47fa4be32e54c80daf8b8d8ebb829799d0
Felix Valdez <fdvaldez14@gmail.com>
2023-11-17 11:40:39 +0100

    add theme variable

--- Scrumdinger/Scrumdinger/Views/MeetingHeaderView.swift
+++ Scrumdinger/Scrumdinger/Views/MeetingHeaderView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MeetingHeaderView: View {
     let secondsElapsed: Int
     let secondsRemaining: Int
+    let theme: Theme
     
     //Totale dei secondi
     private var totalSeconds: Int {
@@ -33,6 +34,7 @@ struct MeetingHeaderView: View {
     var body: some View {
         VStack {
             ProgressView(value: progressBar)
+            
             HStack {
                 VStack(alignment: .leading) {
                     Text("Second Elapsed")
@@ -56,7 +58,7 @@ struct MeetingHeaderView: View {
 
 struct MeetingHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180)
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180, theme: .bubbleGum)
             .previewLayout(.sizeThatFits)  // dimensiona la View dal contenuto
     }
 }
