f4547dda179ec787f59dffdbfbefb16c67bfec1e
Felix Valdez <fdvaldez14@gmail.com>
2023-11-22 20:57:34 +0100

    Add NewScrumView

---
+++ Scrumdinger/Scrumdinger/Views/NewScrumSheet.swift
@@ -0,0 +1,18 @@
+//
+//  NewScrumSheet.swift
+//  Scrumdinger
+//
+//  Created by Felix Valdez on 22/11/23.
+//
+
+import SwiftUI
+
+struct NewScrumSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewScrumSheet()
+}
